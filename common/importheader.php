879288fe3574aaa7597306868b899641ac6f930b
JptmJ <joshiptm797@gmail.com>
2023-10-27 19:09:36 +0530

    First Update

---
+++ common/importheader.php
@@ -0,0 +1,8 @@
+<nav class="navbar">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="assets/images/cte-logo_0.png" alt="Logo" class="d-inline-block align-text-top">
+        </a>
+        <button class="btn btn-primary">Department Login</button>
+    </div>
+</nav>
---
+++ common/importheader2.php
@@ -0,0 +1,53 @@
+<nav class="navbar navbar-expand-lg bg-primary text-light">
+    <div class="container-fluid container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0 color-white">
+                <li class="nav-item home-btn">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Service</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">From Book</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Applications</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">e-Citizen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Multimedia</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Notice Board</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">RTI</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About</a>
+                </li>
+
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-dark" type="submit">Search</button>
+            </form>
+        </div>
+    </div>
+</nav>
