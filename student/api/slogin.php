8d00f0631a400ace048fd0609f31e865f9a648a5
RIjvan-Juneja <junejarijvan8128@gmail.com>
2023-11-04 15:25:36 +0530

    d

--- student/api/slogin.php
+++ student/api/slogin.php
@@ -14,19 +14,22 @@
 
     if ($student_result->num_rows > 0 && $application_result->num_rows > 0) {
         // Both password and enrollment number exist
-        echo "<script>alert('login succesfully');</script>";
+        echo "<script>confirm('login succesfully');</script>";
         $_SESSION['enrollment_number'] = $enrollment_num;
         header('location:../application.php');
 
     } elseif ($student_result->num_rows > 0) {
         // Password exists, but enrollment number does not
-        echo "<script>alert('First, apply for a transfer')</script>";
+        echo '<script>alert("First, apply for a transfer")</script>';
+        header('location:../login.php');
     } elseif ($application_result->num_rows > 0) {
         // Enrollment number exists, but password does not
-        echo "<script>alert('Invalid data')</script>";
+        echo '<script>alert("Invalid data")</script>';
+        header('location:../login.php');
     } else {
         // Neither password nor enrollment number exists
-        echo "<script>alert('Invalid data')</script>";
+        echo '<script>alert("Invalid data")</script>';
+        header('location:../login.php');
     }
 
     // Close the database connection
